d965ea3ac7eadadd3e5bf22a2e30a7528a7a4ee1
moyy <imoyyi@outlook.com>
2024-08-24 16:38:07 +0000

    chore: 汉化....

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ export default defineConfig({
 		smartypants: true,
 		rehypePlugins: [ rehypeExternalLinksConfig ],
 		shikiConfig: {
-			theme: 'github-light-default'
+			theme: 'github-dark'
 		}
 	}
 })
